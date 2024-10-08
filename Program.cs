4149931f87af2380ee635e2f6ab3d48ece416d3c
Marijana <merm2431@gmail.com>
2024-10-08 20:54:11 +0200

    Prilagodba backend za potrebe frontenda

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-
+﻿
 using KnjiznicaAPP.Data;
 using Microsoft.EntityFrameworkCore;
 
@@ -17,6 +17,16 @@ builder.Services.AddDbContext<EdunovaContext>(opcije =>
     opcije.UseSqlServer(builder.Configuration.GetConnectionString("EdunovaContext"));
 });
 
+// Svi se od svuda na sve moguæe naèine mogu spojitina naš API
+// Čitati https://code-maze.com/aspnetcore-webapi-best-practices/
+builder.Services.AddCors(opcije =>
+{
+    opcije.AddPolicy("CorsPolicy",
+        builder =>
+            builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader()
+    );
+
+});
 
 
 var app = builder.Build();
@@ -24,13 +34,13 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 //if (app.Environment.IsDevelopment())
 //{
-   app.UseSwagger();
-    app.UseSwaggerUI(o => {
+app.UseSwagger();
+app.UseSwaggerUI(o => {
 
-        o.ConfigObject.AdditionalItems.Add("requestSnippetsEnabled", true);
-        o.EnableTryItOutByDefault();
+    o.ConfigObject.AdditionalItems.Add("requestSnippetsEnabled", true);
+    o.EnableTryItOutByDefault();
 
-    });
+});
 //}
 
 app.UseHttpsRedirection();
@@ -38,5 +48,12 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+// za potrebe produkcije
+app.UseStaticFiles();
+app.UseDefaultFiles();
+app.MapFallbackToFile("index.html");
+
+app.UseCors("CorsPolicy");
 
 app.Run();
